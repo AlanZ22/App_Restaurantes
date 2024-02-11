96f23f470658c49c9e44be7a3f3542bad73533ad
AlanZ <97094432+AlanZ22@users.noreply.github.com>
2024-02-11 20:37:03 -0300

    Add files via upload

---
+++ bebida.py
@@ -0,0 +1,13 @@
+from cardapio.item_cardapio import ItemCardapio
+
+
+class Bebida(ItemCardapio):
+    def __init__(self, nome,preco,tamanho):
+        super().__init__(nome,preco)
+        self.tamanho = tamanho
+
+    def __str__(self):
+        return f'{self._nome} | {self._preco} | {self.tamanho}'
+
+    def aplicar_desconto(self):
+        self._preco -= (self._preco * 0.08)
